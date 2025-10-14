ffa2c38dcc4fb23a933c09a7a745020800946068
lotarv <lotarev200445@gmail.com>
2025-10-14 20:11:57 +0300

    feat(App): add image, animation, english locale and toolbar (still need to make change locale in menu)

--- app/src/main/java/com/lotarv/bigsteppa/CalendarScreen.kt
+++ app/src/main/java/com/lotarv/bigsteppa/CalendarScreen.kt
@@ -1,24 +1,21 @@
 package com.example.steptrackercompose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import kotlin.random.Random
 
 @Composable
-fun CalendarScreen(onBack: () -> Unit) {
+fun CalendarScreen(onBack: () -> Unit, modifier: Modifier) {
     val days = 31
     val steps = remember { List(days) { Random.nextInt(0, 10000) } }
 
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
--- app/src/main/java/com/lotarv/bigsteppa/MainActivity.kt
+++ app/src/main/java/com/lotarv/bigsteppa/MainActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,21 +13,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.steptrackercompose.CalendarScreen
 import com.lotarv.bigsteppa.ui.theme.BigSteppaTheme
 
@@ -59,36 +70,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun StepTrackerScreen(onOpenCalendar: () -> Unit) {
+fun StepTrackerScreen(onOpenCalendar: () -> Unit, modifier: Modifier) {
     var steps by remember { mutableStateOf(0) }
+    val animatedSteps by animateIntAsState(steps)
     var goal by remember { mutableStateOf(5000) }
 
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .padding(24.dp),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Шагомер", style = MaterialTheme.typography.headlineMedium)
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(stringResource(R.string.app_name), style = MaterialTheme.typography.headlineMedium)
+            Image(
+                painter = painterResource(R.drawable.step),
+                contentDescription = "Кроссовок",
+                modifier = Modifier.size(256.dp)
+            )
+        }
 
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
-                text = "$steps",
+                text = "$animatedSteps",
                 style = MaterialTheme.typography.displayLarge
             )
-            Text("шагов из $goal", style = MaterialTheme.typography.bodyLarge)
+            Text("${stringResource(R.string.steps)} из $goal", style = MaterialTheme.typography.bodyLarge)
         }
 
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { steps++ }, modifier = Modifier.fillMaxWidth()) {
-                Text("Сделать шаг")
+                Text(stringResource(R.string.make_step))
             }
 
             Spacer(Modifier.height(8.dp))
 
             Button(onClick = { steps = 0 }, modifier = Modifier.fillMaxWidth()) {
-                Text("Сброс")
+                Text(stringResource(R.string.reset))
             }
 
             Spacer(Modifier.height(8.dp))
@@ -96,13 +115,19 @@ fun StepTrackerScreen(onOpenCalendar: () -> Unit) {
             Button(onClick = {
                 goal = listOf(3000, 5000, 8000, 10000).random()
             }, modifier = Modifier.fillMaxWidth()) {
-                Text("Новое задание")
+                Text(stringResource(R.string.goal))
             }
 
             Spacer(Modifier.height(8.dp))
 
-            Button(onClick = onOpenCalendar, modifier = Modifier.fillMaxWidth()) {
-                Text("Календарь")
+            Button(onClick = onOpenCalendar,
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(R.color.btnBackground),
+                    contentColor = colorResource(R.color.btnText)
+                )
+            ) {
+                Text(stringResource(R.string.calendar))
             }
         }
     }
@@ -110,14 +135,54 @@ fun StepTrackerScreen(onOpenCalendar: () -> Unit) {
 
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StepTrackerApp() {
     var currentScreen by remember { mutableStateOf("main") }
+    var currentLang by remember { mutableStateOf("ru") }
+    var expanded by remember { mutableStateOf(false) }
 
-    when (currentScreen) {
-        "main" -> StepTrackerScreen(
-            onOpenCalendar = { currentScreen = "calendar" }
-        )
-        "calendar" -> CalendarScreen(onBack = { currentScreen = "main" })
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { ' ' },
+                actions = {
+                    IconButton(onClick = { expanded = true }) {
+                        Text("⋮",fontSize = 28.sp, fontWeight = FontWeight.Bold) // три вертикальные точки
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text("Русский") },
+                            onClick = {
+                                currentLang = "ru"
+                                expanded = false
+                            }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("English") },
+                            onClick = {
+                                currentLang = "en"
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        when (currentScreen) {
+            "main" -> StepTrackerScreen(
+                onOpenCalendar = { currentScreen = "calendar" },
+                modifier = Modifier.padding(innerPadding)
+            )
+            "calendar" -> CalendarScreen(
+                onBack = { currentScreen = "main" },
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
+
